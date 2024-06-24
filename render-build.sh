de4768bab0f3db34e1075f8e77212079e1dffa10
Janne Puhakka <jaggeql@gmail.com>
2024-06-24 11:11:03 +0300

    npm run start-prod removed from build script

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,5 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# Install dependencies
 npm install
 
-# Build the frontend
 npm run build
-
-# Start the backend server
-npm run start-prod
